9ba339c0e36779369f828579589e4147b2f4a63f
DarrenChou <darrenchoucn@gmail.com>
2026-08-21 14:32:21 +1000

    Complete Bootstrap Vue registration form

--- szhou-library/src/App.vue
+++ szhou-library/src/App.vue
@@ -1,14 +1,7 @@
 <script setup>
-import JSONLab from './components/JSON.vue'
+import LibraryRegistrationForm from './components/LibraryRegistrationForm.vue'
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-  <JSONLab />
+  <LibraryRegistrationForm />
 </template>
-
-<style scoped></style>
